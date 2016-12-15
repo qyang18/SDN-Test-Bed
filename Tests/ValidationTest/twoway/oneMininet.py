cfb500678428afdcb1e278cd5d393cca8dd6856c
qyang18 <qyang18@hawk.iit.edu>
2016-12-14 22:50:51 -0600

    Update oneMininet.py

--- Tests/ValidationTest/twoway/oneMininet.py
+++ Tests/ValidationTest/twoway/oneMininet.py
@@ -1,42 +1,43 @@
-from mininet.net import Mininet
-from mininet.topolib import TreeTopo
-from mininet.node import RemoteController, OVSSwitch
-from mininet.link import TCLink
-import re
-import sys
-
 from mininet.cli import CLI
-from mininet.log import setLogLevel, info, error
-from mininet.link import Intf
-from mininet.util import quietRun
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.node import RemoteController, OVSSwitch
 
-info("*** Creating controllers\n")
-net = Mininet( controller=RemoteController, switch = OVSSwitch )
+from sys import argv
 
-c1 = net.addController('c1', ip='192.168.33.101', port=6033)
-c2 = net.addController('c2', ip='192.168.33.101', port=6133)
-c3 = net.addController('c3', ip='192.168.33.101', port=6233)
+class SingleSwitchTopo(Topo):
+    "Single switch connected to n hosts."
+    def __init__(self, **opts):
+        Topo.__init__(self, **opts)
 
-info("*** Creating switches\n")
-s1 = net.addSwitch('s1',dpid='1000000000000001')
-s2 = net.addSwitch('s2',dpid='1000000000000002')
+  s1 = self.addSwitch('s1',dpid='1000000000000001')
+	s2 = self.addSwitch('s2',dpid='1000000000000002')
+            
+  h1 = self.addHost('h1',ip='12.0.0.1')
+	h2 = self.addHost('h2',ip='12.0.0.2')
+        
+	self.addLink(h1, s1, bw=100)
+	self.addLink(s1, s2, bw=100)
+	self.addLink(s2, h2, bw=100)
 
-info("*** Creating hosts\n")
-h1 = net.addHost('h1',ip='12.0.0.1')
-h2 = net.addHost('h2',ip='12.0.0.2')
+def perfTest(  ):
+    "Create network and run simple performance test"
+    topo = SingleSwitchTopo()
+    net = Mininet( controller=RemoteController, topo=topo, link=TCLink, switch = OVSSwitch )
+    c1 = net.addController('c1', ip='192.168.33.101', port=6033)
+    c2 = net.addController('c2', ip='192.168.33.101', port=6133)
+    c3 = net.addController('c3', ip='192.168.33.101', port=6233)
+    	
+    net.start()
+    h1, h2 = net.getNodeByName('h1', 'h2')
+    h1.cmd("./Server 5 &")
+    h2.cmd("./Client 5 11.1.0.1 ")
+    #CLI(net)
+    net.stop()
 
-info("*** Creating links\n")
-link11 = net.addLink(s1,h1,cls=TCLink)
-link11.intf1.config(bw=100)
-link12 = net.addLink(s1,s2,cls=TCLink)
-link12.intf1.config(bw=100)
-link22 = net.addLink(s2,h2,cls=TCLink)
-link22.intf1.config(bw=100)
-
-#net.addLink(host, switch, bw=10, delay='5ms', loss=10, use_htb=True)
-
-info("*** Starting network\n")
-net.build()
-net.start()
-CLI(net)
-net.stop()
+setLogLevel( 'info' )
+perfTest( )
